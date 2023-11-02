2e25794860f1869f3d4013b2b26178032a70ef10
cloudninefalling <shalygin.da7@gmail.com>
2023-11-02 23:13:58 +0300

    fix: cors allow-origin debug

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,7 +9,7 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin');
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   const requestHeaders = req.headers['access-control-request-headers'];
